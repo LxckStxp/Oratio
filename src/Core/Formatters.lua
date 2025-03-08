443bb75dd5d9b08d4951b22600f5454b8fcb4725
LxckStxp <lxckstxp@gmail.com>
2025-03-08 20:01:55 +1000

    Update Formatters.lua

--- src/Core/Formatters.lua
+++ src/Core/Formatters.lua
@@ -4,18 +4,17 @@ local Formatters = {}
 -- StringUtils will be accessed from the global table
 local StringUtils
 
-function Formatters.default(config, level, message, timestamp, ...)
+function Formatters.default(config, level, message, ...)
     local formattedMessage = StringUtils.format(message, ...)
     return string.format(
-        "[%s] [%s] [%s]: %s",
-        os.date("%H:%M:%S", timestamp),
+        "[%s] [%s]: %s",
         config.moduleName,
         level,
         formattedMessage
     )
 end
 
-function Formatters.compact(config, level, message, timestamp, ...)
+function Formatters.compact(config, level, message, ...)
     local formattedMessage = StringUtils.format(message, ...)
     return string.format(
         "[%s-%s]: %s",
@@ -25,11 +24,10 @@ function Formatters.compact(config, level, message, timestamp, ...)
     )
 end
 
-function Formatters.json(config, level, message, timestamp, ...)
+function Formatters.json(config, level, message, ...)
     local formattedMessage = StringUtils.format(message, ...)
     return string.format(
-        '{"timestamp":"%s","module":"%s","level":"%s","message":"%s"}',
-        os.date("%Y-%-m-%dT%H:%M:%SZ", timestamp),
+        '{"module":"%s","level":"%s","message":"%s"}',
         config.moduleName,
         level,
         formattedMessage
